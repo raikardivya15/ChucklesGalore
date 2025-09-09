6bb51bf7edb01b9a8decc5af83f7a403cd4351c3
Divya Raikar <110682875+raikardivya15@users.noreply.github.com>
2025-09-09 23:52:00 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ChucklesGalore/", // MUST match repo name
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/ChucklesGalore/', // for GitHub
-})
+});
