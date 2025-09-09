a8bdd757cb6f5ed7435c8dc3aa4c91b742863d5e
Divya Raikar <110682875+raikardivya15@users.noreply.github.com>
2025-09-09 23:36:18 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  base: '/ChucklesGalore/', // for GitHub
+  base: '/ChucklesGalore/', // Must match your GitHub repo name
 })
+
